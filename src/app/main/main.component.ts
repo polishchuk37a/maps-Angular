d8ec11044df852ea3120f897590d9754f89184e8
Polishchuk <a_polishchuk@mifprojects.com>
2022-10-17 11:31:36 +0300

    feat: add map

--- src/app/main/main.component.ts
+++ src/app/main/main.component.ts
@@ -1,4 +1,5 @@
-import { Component, OnInit } from '@angular/core';
+import {Component, OnInit} from '@angular/core';
+import * as L from 'leaflet';
 
 @Component({
   selector: 'app-main',
@@ -6,10 +7,21 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./main.component.scss']
 })
 export class MainComponent implements OnInit {
+  map: L.Map;
 
   constructor() { }
 
   ngOnInit(): void {
+    this.initMap();
+  }
+
+  initMap(): void {
+    this.map = L.map('map').setView([48.5132, 32.2597], 12);
+
+    L.tileLayer('https://tile.openstreetmap.org/{z}/{x}/{y}.png', {
+      maxZoom: 19,
+      attribution: '&copy; <a href="http://www.openstreetmap.org/copyright">OpenStreetMap</a>'
+    }).addTo(this.map);
   }
 
 }
