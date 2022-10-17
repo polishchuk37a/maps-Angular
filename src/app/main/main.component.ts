e9ce75746fc22bcded6730651d9ee9aa69d9b5bf
Polishchuk <a_polishchuk@mifprojects.com>
2022-10-17 16:36:18 +0300

    feat: add possibility to see all marks on the map, add custom icon for markers

--- src/app/main/main.component.ts
+++ src/app/main/main.component.ts
@@ -1,6 +1,7 @@
 import {Component, OnInit} from '@angular/core';
 import * as L from 'leaflet';
 import {FormBuilder} from "@angular/forms";
+import {featureGroup, Layer, marker} from "leaflet";
 
 @Component({
   selector: 'app-main',
@@ -9,6 +10,13 @@ import {FormBuilder} from "@angular/forms";
 })
 export class MainComponent implements OnInit {
   map: L.Map;
+  markers: Layer[] = [];
+  defaultCoordinates = {x: 48.5132, y: 32.2597};
+
+  markerIcon = L.icon({
+    iconUrl: 'assets/images/ukrainian-flag.png',
+    iconSize: [70, 80]
+  })
 
   coordinateForm = this.formBuilder.group({
     coordinateX: [''],
@@ -22,10 +30,12 @@ export class MainComponent implements OnInit {
   }
 
   initMap(): void {
-    const defaultCoordinates = L.latLng(48.5132, 32.2597);
+    const startCoordinates = L.latLng(this.defaultCoordinates.x, this.defaultCoordinates.y);
 
-    this.map = L.map('map').setView(defaultCoordinates, 12);
-    L.marker(defaultCoordinates).addTo(this.map);
+    this.map = L.map('map').setView(startCoordinates, 12);
+
+    this.markers.push(marker([this.defaultCoordinates.x, this.defaultCoordinates.y], {icon: this.markerIcon}));
+    L.marker(startCoordinates, {icon: this.markerIcon}).addTo(this.map);
 
     L.tileLayer('https://tile.openstreetmap.org/{z}/{x}/{y}.png', {
       maxZoom: 15,
@@ -35,9 +45,13 @@ export class MainComponent implements OnInit {
   setViewAndMarkByCoordinates(): void {
     const x = this.coordinateForm.get('coordinateX')?.value;
     const y = this.coordinateForm.get('coordinateY')?.value;
-    const coordinates = L.latLng(x, y);
 
-    this.map.setView(coordinates, 12);
-    L.marker(coordinates).addTo(this.map);
+    this.markers.push(marker([x, y], {icon: this.markerIcon}));
+
+    const group = featureGroup(this.markers);
+
+    group.addTo(this.map);
+    this.map.fitBounds(group.getBounds());
   }
+
 }
