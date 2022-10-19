016c705ef95cda8315b5dccbce0afee9bd4ba15a
Polishchuk <a_polishchuk@mifprojects.com>
2022-10-19 20:14:51 +0300

    feat(app): add possibility to paste coordinates into coordinates form using keyboard combination

--- src/app/main/main.component.ts
+++ src/app/main/main.component.ts
@@ -1,34 +1,64 @@
-import {Component, OnInit} from '@angular/core';
+import {AfterViewInit, Component, ElementRef, OnDestroy, OnInit, ViewChild} from '@angular/core';
 import * as L from 'leaflet';
+import {featureGroup, Layer, marker} from 'leaflet';
 import {FormBuilder} from "@angular/forms";
-import {featureGroup, Layer, marker} from "leaflet";
 import 'leaflet-draw';
+import {fromEvent, Subject} from "rxjs";
+import {takeUntil, tap} from "rxjs/operators";
 
 @Component({
   selector: 'app-main',
   templateUrl: './main.component.html',
   styleUrls: ['./main.component.scss']
 })
-export class MainComponent implements OnInit {
+export class MainComponent implements OnInit, AfterViewInit, OnDestroy {
   map: L.Map;
   markers: Layer[] = [];
   defaultCoordinates = {x: 48.5132, y: 32.2597};
 
   markerIcon = L.icon({
     iconUrl: 'assets/images/ukrainian-flag.png',
-    iconSize: [70, 80]
-  })
+    iconSize: [50, 60]
+  });
 
   coordinateForm = this.formBuilder.group({
     coordinateX: [''],
     coordinateY: ['']
   });
 
+  @ViewChild('coordinateXInput') coordinateXInput: ElementRef;
+
+  private unsubscribe$ = new Subject<void>();
+
   constructor(private readonly formBuilder: FormBuilder) { }
 
   ngOnInit(): void {
     this.initMap();
     this.drawOnTheMap();
+  }
+
+  readDataFromClipBoard(): void {
+    navigator.clipboard.readText().then((value) => {
+      this.splitCoordinates(value);
+    });
+  }
+
+  splitCoordinates(valueFromClipboard: string): void {
+    const coordinates = {
+      coordinateX: '',
+      coordinateY: ''
+    };
+
+    const coordinatesArray = valueFromClipboard.split('').filter(value => value !== ',').join('').split(' ', 2);
+    coordinates.coordinateX = coordinatesArray[0];
+    coordinates.coordinateY = coordinatesArray[1];
+
+    this.setCoordinatesIntoForm(coordinates.coordinateX, coordinates.coordinateY);
+  }
+
+  setCoordinatesIntoForm(coordinateX: string, coordinateY: string): void {
+    this.coordinateForm.get('coordinateX')?.setValue(coordinateX);
+    this.coordinateForm.get('coordinateY')?.setValue(coordinateY);
   }
 
   initMap(): void {
@@ -64,7 +94,7 @@ export class MainComponent implements OnInit {
         marker: false,
         polyline: false
       },
-      position: "topright"
+      position: "topright",
     });
 
     this.map.addControl(drawControl);
@@ -86,4 +116,20 @@ export class MainComponent implements OnInit {
     this.map.fitBounds(group.getBounds());
   }
 
+  ngAfterViewInit(): void {
+    fromEvent<KeyboardEvent>(this.coordinateXInput.nativeElement, 'keydown')
+      .pipe(
+        tap((e: KeyboardEvent) => {
+          if (e.keyCode === 86 && e.ctrlKey) {
+            this.readDataFromClipBoard();
+          }
+        }),
+        takeUntil(this.unsubscribe$)
+      ).subscribe();
+  }
+
+  ngOnDestroy(): void {
+    this.unsubscribe$.next();
+    this.unsubscribe$.complete();
+  }
 }
