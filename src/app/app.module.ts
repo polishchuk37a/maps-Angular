08f48afa477d55f80c368b1ba24e1ceb4105508b
Polishchuk <a_polishchuk@mifprojects.com>
2022-11-27 15:06:16 +0200

    feat: install angular material into project, add styles for template elements

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,16 +3,24 @@ import { BrowserModule } from '@angular/platform-browser';
 import { AppComponent } from './app.component';
 import {ReactiveFormsModule} from "@angular/forms";
 import { MapComponent } from './map/map.component';
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+import {MatButtonModule} from "@angular/material/button";
+import {MatFormFieldModule} from "@angular/material/form-field";
+import {MatInputModule} from "@angular/material/input";
 
 @NgModule({
   declarations: [
     AppComponent,
     MapComponent
   ],
-    imports: [
-        BrowserModule,
-        ReactiveFormsModule,
-    ],
+  imports: [
+    BrowserModule,
+    ReactiveFormsModule,
+    BrowserAnimationsModule,
+    MatButtonModule,
+    MatFormFieldModule,
+    MatInputModule,
+  ],
   providers: [],
   bootstrap: [AppComponent]
 })
